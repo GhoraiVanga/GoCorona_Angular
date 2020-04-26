577b8d41291bcf4d6ca0fa6b5e2064a892219d30
GhoraiVanga <48147708+GhoraiVanga@users.noreply.github.com>
2020-04-26 14:31:17 +0530

    deploy on corona-tracker-suman

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Covid19Component } from './covid19/covid19.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {HttpClientModule} from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -12,7 +20,9 @@ import { Covid19Component } from './covid19/covid19.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    HttpClientModule,MatPaginatorModule,MatSortModule,MatInputModule,MatFormFieldModule,MatButtonModule,MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/countryReports.ts
@@ -0,0 +1,17 @@
+export interface CountryReports
+{ 
+    country :string;
+    cases :number;
+    todayCases:number;
+    deaths:string;
+    todayDeaths :string;
+    recovered:number;
+    active:number;
+    critical:string;
+    casesPerOneMillion:number;
+   
+    tests :string;
+    testsPerOneMillion:string;
+
+
+}
