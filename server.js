0a2a49422ba63733c80d0e5e98057073736d3278
Suman Ghorai <48147708+GhoraiVanga@users.noreply.github.com>
2020-05-08 23:46:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/corona-tracker-suman'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/angular-on-heroku/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8090);
